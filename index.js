b52fd07590756b137a43b98bf5054e13ab3c7c0d
vansh kushwaha <108723120+vanshkushwaha8@users.noreply.github.com>
2025-06-28 22:02:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['https://zerofy.netlify.app', 'http://localhost:3000'];
+const allowedOrigins = ['https://zerofy-tawny.vercel.app', 'http://localhost:5173','*'];
 
 app.use(cors({
   origin: function (origin, callback) {
